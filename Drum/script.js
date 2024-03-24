897c8a3f15232d05192d7ecda865137fbf361c6e
Ritesh Giri <rrgiri032@gmail.com>
2024-03-24 16:57:34 +0530

    Drum Kit

--- Drum/script.js
+++ Drum/script.js
@@ -1,32 +1,46 @@
-const data = [
-  { title: "CLAP", h1: "A", key: 65 },
-  { title: "HIHAT", h1: "S", key: 83 },
-  { title: "KICK", h1: "D", key: 68 },
-  { title: "OPENHAT", h1: "F", key: 70 },
-  { title: "BOOM", h1: "G", key: 71 },
-  { title: "RIDE", h1: "H", key: 72 },
-  { title: "SNARE", h1: "J", key: 74 },
-  { title: "TOM", h1: "K", key: 75 },
-  { title: "TONY", h1: "L", key: 76 },
-];
+// const data = [
+//   { title: "CLAP", h1: "A", key: 65 },
+//   { title: "HIHAT", h1: "S", key: 83 },
+//   { title: "KICK", h1: "D", key: 68 },
+//   { title: "OPENHAT", h1: "F", key: 70 },
+//   { title: "BOOM", h1: "G", key: 71 },
+//   { title: "RIDE", h1: "H", key: 72 },
+//   { title: "SNARE", h1: "J", key: 74 },
+//   { title: "TOM", h1: "K", key: 75 },
+//   { title: "TONY", h1: "L", key: 76 },
+// ];
 
-function drum() {
-  const drumKick = document.querySelector(".drum-kicks");
+// function drum() {
+//   const drumKick = document.querySelector(".drum-kicks");
 
-  let clutter = "";
+//   let clutter = "";
 
-  data.forEach(function (val) {
-    clutter += `<div class="keys">
-  <h1>${val.h1}</h1>
-  <span data-key="${val.key}" class="title">${val.title}</span>
-</div>`;
-  });
+//   data.forEach(function (val) {
+//     clutter += `<div class="keys">
+//   <h1>${val.h1}</h1>
+//   <span data-key="${val.key}" class="title">${val.title}</span>
+// </div>`;
+//   });
 
-  drumKick.innerHTML = clutter;
-}
-drum();
+//   drumKick.innerHTML = clutter;
+// }
+// drum();
 
 window.addEventListener("keydown", function (e) {
-  // console.log(e.keyCode);
-  const audio = document.querySelectorAll(`"audio[${data.key}]"`);
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const keys = document.querySelector(`.keys[data-key="${e.keyCode}"]`);
+
+  if (!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+
+  keys.classList.add("play");
+
+  keys.addEventListener("transitionend", function () {
+    keys.classList.remove("play");
+  });
+
+  // audio.addEventListener("ended", function () {
+  //   keys.classList.remove("play");
+  // });
 });
