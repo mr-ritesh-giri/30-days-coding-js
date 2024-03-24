ba15f8111c1eb312007e079135571f647c5d885e
Ritesh Giri <rrgiri032@gmail.com>
2024-03-24 18:40:26 +0530

    //

---
+++ 2. CSS + JS Clock/script.js
@@ -0,0 +1,26 @@
+const second = document.querySelector(".second-hand");
+const minute = document.querySelector(".minute-hand");
+const hour = document.querySelector(".hour-hand");
+
+function setDate() {
+  const now = new Date();
+
+  // Seconds
+  const seconds = now.getSeconds();
+  const secondDegree = (seconds / 60) * 360 + 90;
+  second.style.transform = `rotate(${secondDegree}deg)`;
+  console.log("seconds", seconds);
+
+  // Minutes
+  const minutes = now.getMinutes();
+  const minuteDegree = (minutes / 60) * 360 + 90;
+  minute.style.transform = `rotate(${minuteDegree}deg)`;
+  console.log("minute", minutes);
+
+  // Hours
+  const hours = now.getHours();
+  const hourDegree = ((hours % 12) / 12) * 360 + (minutes / 60) * 30 + 90;
+  hour.style.transform = `rotate(${hourDegree}deg)`;
+  console.log("hours", hours);
+}
+setInterval(setDate, 1000);
