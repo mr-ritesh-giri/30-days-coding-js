2ed1b05e76e9728e4fbad9cca5ae8eeb8a5761bc
Ritesh Giri <rrgiri032@gmail.com>
2024-03-28 11:17:58 +0530

    JS Clock

--- 2. CSS + JS Clock/script.js
+++ 2. CSS + JS Clock/script.js
@@ -2,25 +2,25 @@ const second = document.querySelector(".second-hand");
 const minute = document.querySelector(".minute-hand");
 const hour = document.querySelector(".hour-hand");
 
-function setDate() {
+function setClock() {
   const now = new Date();
 
   // Seconds
   const seconds = now.getSeconds();
-  const secondDegree = (seconds / 60) * 360 + 90;
+  const secondDegree = (seconds / 60) * 360 - 90;
   second.style.transform = `rotate(${secondDegree}deg)`;
   console.log("seconds", seconds);
 
   // Minutes
   const minutes = now.getMinutes();
-  const minuteDegree = (minutes / 60) * 360 + 90;
+  const minuteDegree = (minutes / 60) * 360 - 90;
   minute.style.transform = `rotate(${minuteDegree}deg)`;
   console.log("minute", minutes);
 
   // Hours
   const hours = now.getHours();
-  const hourDegree = ((hours % 12) / 12) * 360 + (minutes / 60) * 30 + 90;
+  const hourDegree = (hours / 12) * 360 - 90 + (minutes / 60) * 30;
   hour.style.transform = `rotate(${hourDegree}deg)`;
   console.log("hours", hours);
 }
-setInterval(setDate, 1000);
+setInterval(setClock, 1000);
